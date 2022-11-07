Fixed variance in UCB

src/losses/losses.py
@@ -14,6 +14,6 @@ class PendulumError():
         config.kp_bo = k[0]
         config.kd_bo = k[1]
         X_bo = simulate(config, dynamics_real, U_bo)
-        norm = np.linalg.norm(self.X_star - X_bo)/self.c.n_simulation
+        norm = np.linalg.norm(self.X_star - X_bo)/np.sqrt(self.c.n_simulation)
 
         return [torch.tensor([k[0], k[1]]), -norm, X_bo]

src/optim/ucb_aquisition.py
@@ -1,10 +1,10 @@
 import numpy as np
 from src.optim.base_aquisition import BaseAquisition
+import torch
 
 class UCBAquisition(BaseAquisition):
     def __init__(self, model, xNormalizer, t, c, logger):
         super().__init__(model, xNormalizer, t, c, logger)
 
     def loss(self, x):
-        return x.mean + np.sqrt(self.c.beta) * self.c.scale_beta * x.variance
-
+        return x.mean + self.c.scale_beta*torch.sqrt(self.c.beta*x.variance)